e86c2fc3ec3a2dc564d77427890370e2daa71687
Bogdan Litescu <bogdan.litescu@avatar-soft.ro>
2013-05-01 23:27:08 +0800

    implemented ISearchable

--- avt.DynamicFlashRotator.Dnn/DynamicRotatorController.cs
+++ avt.DynamicFlashRotator.Dnn/DynamicRotatorController.cs
@@ -12,10 +12,12 @@ using avt.DynamicFlashRotator.Net.RegCore.Storage;
 using avt.DynamicFlashRotator.Net.RegCore;
 using DotNetNuke.Entities.Portals;
 using System.Collections;
+using DotNetNuke.Services.Search;
+using DotNetNuke.Common.Utilities;
 
 namespace avt.DynamicFlashRotator.Dnn
 {
-    public class DynamicRotatorController : IPortable, IUpgradeable
+    public class DynamicRotatorController : IPortable, IUpgradeable, ISearchable
     {
 
         #region RegCore
@@ -120,6 +122,45 @@ namespace avt.DynamicFlashRotator.Dnn
 
             return "Done";
         }
+
+        public SearchItemInfoCollection GetSearchItems(ModuleInfo ModInfo)
+        {
+            RotatorSettings.Init(new DnnConfiguration());
+            RotatorSettings settings = new RotatorSettings();
+            settings.LoadFromDB(ModInfo.ModuleID.ToString());
+
+            var SearchItemCollection = new SearchItemInfoCollection();
+
+            foreach (SlideInfo slide in settings.Slides)
+                SearchItemCollection.Add(IndexSlide(ModInfo, slide));
+
+            return SearchItemCollection;
+        }
+
+        SearchItemInfo IndexSlide(ModuleInfo ModInfo, SlideInfo slide)
+        {
+            var title = slide.Title;
+            if (string.IsNullOrEmpty(title))
+                title = "Unnamed slide";
+
+            var sb = new StringBuilder();
+            foreach (SlideObjectInfo obj in slide.SlideObjects) {
+                if (!string.IsNullOrEmpty(obj.Text))
+                    sb.AppendFormat("{0} ", obj.Text);
+            }
+
+            return new SearchItemInfo(
+                title,
+                sb.ToString().Substring(0, Math.Max(100, sb.Length)), // description
+                -1, // objContent.LastModifiedByUserID,
+                DateTime.Now, // objContent.LastModifiedOnDate,
+                ModInfo.ModuleID,
+                slide.Id.ToString(),
+                title + " " + sb.ToString(),
+                "",
+                Null.NullInteger);
+        }
+
     }
 }
 
